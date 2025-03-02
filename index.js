fe4e72ccb66e2540fd3efabd5c95dcbcd6fc21e3
mobin afshari <mma137813781378@gmail.com>
2025-03-02 20:58:08 +0330

    fix: some of problems fixed and also published in npm library and is ready to use :)

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const fs = require("fs-extra"); 
+#!/usr/bin/env node
+const fs = require("fs-extra");
 const path = require("path");
 const parser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
-const generator = require("@babel/generator").default;
 
-const folderPath = "./src";
-const allClasses = new Set(); 
+const userProjectDir = process.cwd();
+const folderPath = process.argv[2] || "src"; 
+const fullFolderPath = path.resolve(userProjectDir, folderPath);
+const allClasses = new Set();
 
 async function processFile(filePath) {
-  const code = await fs.readFile(filePath, "utf-8"); 
+  const code = await fs.readFile(filePath, "utf-8");
   const ast = parser.parse(code, {
-    sourceType: "module", 
-    plugins: ["jsx"], 
+    sourceType: "module",
+    plugins: ["jsx" , "typescript"],
   });
-
-  const customClasses = new Set(); 
 
   traverse(ast, {
     JSXAttribute(path) {
@@ -27,33 +27,31 @@ async function processFile(filePath) {
           const classes = classValue.value.split(/\s+/);
           classes.forEach((cls) => {
             if (/\[.*?\]/.test(cls)) {
-              customClasses.add(cls);
               allClasses.add(cls);
             }
           });
         }
 
-          if (classValue.type === "JSXExpressionContainer") {
-            const expression = classValue.expression;
-            if (expression.type === "TemplateLiteral") {
-              let fullClass = "";
+        if (classValue.type === "JSXExpressionContainer") {
+          const expression = classValue.expression;
+          if (expression.type === "TemplateLiteral") {
+            let fullClass = "";
 
-              expression.quasis.forEach((quasi, index) => {
-                fullClass += quasi.value.raw;
-                if (expression.expressions[index]) {
-                  fullClass += `[${expression.expressions[index].name}]`;
-                }
-              });
+            expression.quasis.forEach((quasi, index) => {
+              fullClass += quasi.value.raw;
+              if (expression.expressions[index]) {
+                fullClass += `[${expression.expressions[index].name}]`;
+              }
+            });
 
-              const classes = fullClass.split(/\s+/);
-              classes.forEach((cls) => {
-                if (/\[.*?\]/.test(cls)) {
-                  customClasses.add(cls);
-                  allClasses.add(cls);
-                }
-              });
-            }
+            const classes = fullClass.split(/\s+/);
+            classes.forEach((cls) => {
+              if (/\[.*?\]/.test(cls)) {
+                allClasses.add(cls);
+              }
+            });
           }
+        }
       }
     },
   });
@@ -61,27 +59,29 @@ async function processFile(filePath) {
 
 async function processFolder(folderPath) {
   const files = await fs.readdir(folderPath);
-
   for (const file of files) {
     const filePath = path.join(folderPath, file);
     const stat = await fs.stat(filePath);
-
-    if (stat.isFile() && /\.(js|jsx|tsx)$/.test(file)) {
+    if (stat.isDirectory()) {
+      await processFolder(filePath);
+    } else if (stat.isFile() && /\.(js|jsx|tsx)$/.test(file)) {
       await processFile(filePath);
     }
   }
 
   if (allClasses.size > 0) {
-    fs.writeFileSync(
-      "output/extracted-classes.json",
-      JSON.stringify([...allClasses], null, 2)
-    );
-    console.log(
-      "All custom classes saved to extracted-classes.json"
-    );
-  }
+  const outputDir = "output";
+
+  fs.ensureDirSync(outputDir);
+
+  const outputPath = path.join(outputDir, "extracted-classes.json");
+
+  fs.writeFileSync(outputPath, JSON.stringify([...allClasses], null, 2));
+
+  console.log("All custom classes saved to extracted-classes.json");
+}
 }
 
-processFolder(folderPath)
+processFolder(fullFolderPath)
   .then(() => console.log("✅ All files processed!"))
   .catch((err) => console.error("❌ Error:", err));
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs"; 
 import babel from "@rollup/plugin-babel"; 
 import { terser } from "rollup-plugin-terser"; 
-
+import json from "@rollup/plugin-json";
 const packageJson = require("./package.json");
 
 export default {
@@ -15,11 +15,11 @@ export default {
   plugins: [
     resolve(), 
     commonjs(), 
+     json(),
     babel({
       babelHelpers: "bundled",
       exclude: "node_modules/**", 
     }),
     terser(), 
   ],
-  external: ["fs-extra", "path"], 
 };
